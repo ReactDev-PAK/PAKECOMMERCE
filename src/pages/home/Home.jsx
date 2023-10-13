edb075ea7506b666121a7d79e6feadc08adb83b9
syedtayyabsagheer <syedtayyabsagheer@gmail.com>
2023-10-13 09:11:03 +0500

    Add Filter

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useContext }  from 'react'
 import Layout from '../../components/layout/Layout'
 import MyContext from '../../context/myContext'
 import HeroSection from '../../components/heroSection/HeroSection';
+import Filter from '../../components/filter/Filter';
 
 function Home() {
   const context = useContext(MyContext);
   return (
     <Layout>
        <HeroSection/>
+       <Filter/>
     </Layout>
   )
 }
