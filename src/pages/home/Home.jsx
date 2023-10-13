ff7ed88d94b4989d21174999fdf31fae62152adc
syedtayyabsagheer <syedtayyabsagheer@gmail.com>
2023-10-13 09:03:40 +0500

    Make the hero component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,13 @@
 import React, { useContext }  from 'react'
 import Layout from '../../components/layout/Layout'
 import MyContext from '../../context/myContext'
+import HeroSection from '../../components/heroSection/HeroSection';
 
 function Home() {
   const context = useContext(MyContext);
- 
-
-
   return (
     <Layout>
-       <h1>Home</h1>
+       <HeroSection/>
     </Layout>
   )
 }
